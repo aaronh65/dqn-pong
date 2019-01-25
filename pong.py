413e200d551f4f77a7420060bc0c544b8c226981
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2019-01-25 23:27:17 +0530

    Removed Mario code

--- pong.py
+++ pong.py
@@ -29,54 +29,54 @@ EPS = 0.01
 
 
 def main(env):
-    ### 首先要為隨時間改變的參數設定schedule
-    # This is a just rough estimate
-    num_iterations = float(40000000) / 4.0
+	### 首先要為隨時間改變的參數設定schedule
+	# This is a just rough estimate
+	num_iterations = float(40000000) / 4.0
 
 
-    # define exploration schedule
-    exploration_schedule = LinearSchedule(1000000, 0.1)
+	# define exploration schedule
+	exploration_schedule = LinearSchedule(1000000, 0.1)
 
 
-    # optimizer
-    OptimizerSpec = namedtuple("OptimizerSpec", ["constructor", "kwargs"])
+	# optimizer
+	OptimizerSpec = namedtuple("OptimizerSpec", ["constructor", "kwargs"])
 
-    optimizer = OptimizerSpec(
-        constructor=optim.RMSprop,
-        kwargs=dict(lr=LEARNING_RATE, alpha=ALPHA, eps=EPS),
-    )
+		optimizer = OptimizerSpec(
+		constructor=optim.RMSprop,
+		kwargs=dict(lr=LEARNING_RATE, alpha=ALPHA, eps=EPS),
+		)
 
 
-    learning(
-        env=env,
-        q_func=DQN,
-        optimizer_spec=optimizer,
-        exploration=exploration_schedule,
-        replay_buffer_size=REPLAY_BUFFER_SIZE,
-        batch_size=BATCH_SIZE,
-        gamma=GAMMA,
-        learning_starts=LEARNING_STARTS,
-        learning_freq=LEARNING_FREQ,
-        frame_history_len=FRAME_HISTORY_LEN,
-        target_update_freq=TARGET_UPDATE_FREQ
-    )
+	learning(
+		env=env,
+		q_func=DQN,
+		optimizer_spec=optimizer,
+		exploration=exploration_schedule,
+		replay_buffer_size=REPLAY_BUFFER_SIZE,
+		batch_size=BATCH_SIZE,
+		gamma=GAMMA,
+		learning_starts=LEARNING_STARTS,
+		learning_freq=LEARNING_FREQ,
+		frame_history_len=FRAME_HISTORY_LEN,
+		target_update_freq=TARGET_UPDATE_FREQ
+		)
 
 
 if __name__ == '__main__':
 
-    env = gym.make("PongNoFrameskip-v4")
+	env = gym.make("PongNoFrameskip-v4")
 
-    # set global seeds
-    env.seed(SEED)
-    torch.manual_seed(SEED)
-    np.random.seed(SEED)
-    random.seed(SEED)
+	# set global seeds
+	env.seed(SEED)
+	torch.manual_seed(SEED)
+	np.random.seed(SEED)
+	random.seed(SEED)
 
-    # monitor & wrap the game
-    env = wrap_dqn(env)
+	# monitor & wrap the game
+	env = wrap_dqn(env)
 
-    expt_dir = 'video/gym-reslults'
-    env = wrappers.Monitor(env, expt_dir, force=True, video_callable=lambda count: count % 50 == 0)
+	expt_dir = 'video/gym-reslults'
+	env = wrappers.Monitor(env, expt_dir, force=True, video_callable=lambda count: count % 50 == 0)
 
-    # main
-    main(env)
+	# main
+	main(env)
